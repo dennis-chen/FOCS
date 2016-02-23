6a771b7f26213ccdf1fe3b657d8ef9e0ae0bec3d
Dennis Chen <dennis.chen@students.olin.edu>
2016-02-22 19:33:15 -0500

    finished q3

--- hw5/homework5.ml
+++ hw5/homework5.ml
@@ -334,7 +334,8 @@ let tm_q2_b = { states = ["start";"acc";"rej";"q1";"q2";"q3";"q4";"q5";"q6";"q7"
 
 let binaryAddition = { states = ["start";"acc";"rej";
     "q1";"q2";"q3";"q4";"q5";"q6";"q7";"q8";"q9";
-    "q10";"q11";"q12";"q13";"q14";"q15";"q16";"q17";"q18"];
+    "q10";"q11";"q12";"q13";"q14";"q15";"q16";"q17";"q18";
+    "a1";"a2";"a3";"a4";"a5";"a6"];
     input_alphabet = ["0";"1";"#"];
     tape_alphabet = ["0";"1";"#";"X";">";"_"];
 		blank = "_";
@@ -343,7 +344,34 @@ let binaryAddition = { states = ["start";"acc";"rej";
 		accept = "acc";
 		reject = "rej";
      delta = (fun inp -> match inp with
-     | ("start", ">") -> ("q2", ">", 1)
+     | ("start", ">") -> ("a1", ">", 1)
+       (*  check first char *)
+     | ("a1", "0") -> ("a2", "0", 1)
+     | ("a1", "1") -> ("a2", "1", 1)
+     | ("a1", c) -> ("rej",c, 1)
+       (* eat up 1s and 0s until first # *)
+     | ("a2", "0") -> ("a2", "0", 1)
+     | ("a2", "1") -> ("a2", "1", 1)
+     | ("a2", "#") -> ("a3", "#", 1)
+     | ("a2", c) -> ("rej",c, 1)
+       (*  check first char *)
+     | ("a3", "0") -> ("a4", "0", 1)
+     | ("a3", "1") -> ("a4", "1", 1)
+     | ("a3", c) -> ("rej",c, 1)
+       (* eat up 1s and 0s until second # *)
+     | ("a4", "0") -> ("a4", "0", 1)
+     | ("a4", "1") -> ("a4", "1", 1)
+     | ("a4", "#") -> ("a5", "#", 1)
+     | ("a4", c) -> ("rej",c, 1)
+       (*  check first char *)
+     | ("a5", "0") -> ("a6", "0", 1)
+     | ("a5", "1") -> ("a6", "1", 1)
+     | ("a5", c) -> ("rej",c, 1)
+       (* eat up 1s and 0s until _ *)
+     | ("a6", "0") -> ("a6", "0", 1)
+     | ("a6", "1") -> ("a6", "1", 1)
+     | ("a6", "_") -> ("q1", "_", 0) (* jump to rewind *)
+     | ("a6", c) -> ("rej",c, 1)
        (* Rewind *)
      | ("q1", "0") -> ("q1", "0", 0)
      | ("q1", "1") -> ("q1", "1", 0)
